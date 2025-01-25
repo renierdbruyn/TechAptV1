438af31e0020731bff24a24fe8a4d20f59241e9e
Renier de Bruyn <renierdbruyn@hotmail.com>
2025-01-25 14:40:39 +0200

    implement task 1

--- TechAptV1.Client/Services/ThreadingService.cs
+++ TechAptV1.Client/Services/ThreadingService.cs
@@ -1,5 +1,10 @@
 ﻿// Copyright © 2025 Always Active Technologies PTY Ltd
 
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using TechAptV1.Client.Models;
+
 namespace TechAptV1.Client.Services;
 
 /// <summary>
@@ -13,11 +18,22 @@ public sealed class ThreadingService(ILogger<ThreadingService> logger, DataServi
     private int _evenNumbers = 0;
     private int _primeNumbers = 0;
     private int _totalNumbers = 0;
+    public int _stopLimit = 200_000; //10_000_000
+    public int _evenStart = 50_000; //2_500_000
+    [CascadingParameter]
+    public List<Number> _numbersList { get; set; } = new();
+    //private List<int> _numbersList { get; set; } = new();
+
+    [ThreadStatic]
+    private static Random _random = new();
+
+    private readonly object _lock = new();
 
     public int GetOddNumbers() => _oddNumbers;
     public int GetEvenNumbers() => _evenNumbers;
     public int GetPrimeNumbers() => _primeNumbers;
     public int GetTotalNumbers() => _totalNumbers;
+    public int GettopLimit() => _stopLimit;
 
     /// <summary>
     /// Start the random number generation process
@@ -25,7 +41,45 @@ public sealed class ThreadingService(ILogger<ThreadingService> logger, DataServi
     public async Task Start()
     {
         logger.LogInformation("Start");
-        throw new NotImplementedException();
+        var OddRng = new Thread(new ThreadStart(GenerateRandomOddNumbers));
+        var PrimeRng = new Thread(new ThreadStart(GeneratePrimeNumbers));
+        ThreadPool.QueueUserWorkItem((state) =>
+        {
+
+            OddRng.Start();
+            PrimeRng.Start();
+
+            while (true)
+            {
+                lock (_lock)
+                {
+                    if (_numbersList.Count >= _evenStart)
+                    {
+                        break;
+                    }
+                }
+            }
+            logger.LogDebug($"_numbersList.Count: {_numbersList.Count}");
+            var EvenRng = new Thread(new ThreadStart(GenerateRandomEvenNumbers));
+            EvenRng.Start();
+
+            OddRng.Join();
+            PrimeRng.Join();
+            EvenRng.Join();
+            logger.LogDebug($"_numbersList.Count: {_numbersList.Count}");
+        });
+
+
+        return;
+    }
+
+
+    private static void EnsureRandomInstantiated()
+    {
+        if (_random == null)
+        {
+            _random = new Random();
+        }
     }
 
     /// <summary>
@@ -36,4 +90,91 @@ public sealed class ThreadingService(ILogger<ThreadingService> logger, DataServi
         logger.LogInformation("Save");
         throw new NotImplementedException();
     }
+
+    private void GenerateRandomOddNumbers()
+    {
+        EnsureRandomInstantiated();
+
+        while (_numbersList.Count < _stopLimit)
+        //while (_numbersList.Length < _stopLimit)
+        {
+            lock (_lock)
+            {
+                //Random random = new Random();
+                int ans = _random.Next(_stopLimit);
+                if (ans % 2 == 1)
+                {
+                    _numbersList.Add(new() { Value = ans, IsPrime = IsPrime(ans) });
+                    //_numbersList.Add(ans);
+                    _oddNumbers++;
+                    _totalNumbers++;
+                }
+            }
+        }
+    }
+
+    private void GenerateRandomEvenNumbers()
+    {
+        EnsureRandomInstantiated();
+
+        while (_numbersList.Count < _stopLimit)
+        //while (_numbersList.Length < _stopLimit)
+        {
+            lock (_lock)
+            {
+                //Random random = new Random();
+                int ans = _random.Next(_stopLimit);
+                if (ans % 2 == 0)
+                {
+                    _numbersList.Add(new() { Value = ans, IsPrime = IsPrime(ans) });
+                    //_numbersList.Add(ans);
+                    _evenNumbers++;
+                    _totalNumbers++;
+                }
+            }
+        }
+    }
+
+    private void GeneratePrimeNumbers()
+    {
+        EnsureRandomInstantiated();
+
+        while (_numbersList.Count < _stopLimit)
+        //while (_numbersList.Length < _stopLimit)
+        {
+            lock (_lock)
+            {
+                //Random random = new Random();
+                int ans = _random.Next(_stopLimit);
+                if (IsPrime(ans))
+                {
+                    _numbersList.Add(new() { Value = ans, IsPrime = true });
+                    //_numbersList.Add(ans);
+                    _primeNumbers++;
+                    _totalNumbers++;
+                }
+            }
+        }
+    }
+
+    private static bool IsPrime(int n)
+    {
+        if (n > 1)
+        {
+            return Enumerable.Range(1, n).Where(x => n % x == 0)
+                             .SequenceEqual(new[] { 1, n });
+        }
+        return false;
+    }
+
+
 }
+
+
+//public static class Rand
+//{
+//    [ThreadStatic] private static Rand defaultRand;
+//    public static Rand Default => defaultRand ??= new Rand();
+//    // Add extra methods for seeding the static instance...
+//}
+
